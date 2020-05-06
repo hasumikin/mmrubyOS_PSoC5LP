9c2c2660a5215e4d6a8e48bce2f6bd024a599d04
HASUMI Hitoshi <hasumikin@gmail.com>
2020-05-06 15:20:31 +0900

    Hello World!

--- mmrubyOS_PSoC5LP.cydsn/main.c
+++ mmrubyOS_PSoC5LP.cydsn/main.c
@@ -1,54 +1,24 @@
 /*****************************************************************************
+*
+* Project: mmrubyOS
+*
 * File Name: main.c
 *
-* Version: 1.10
+* Version: 0.1.0
 *
-* Description: 
-*  This code example project demonstrates how to communicate between 
-*  the PC and UART component in Full duplex mode. The UART has receiver(RX) and 
-*  transmitter(TX) part. The data received by RX is looped back to the TX.
+* Description:
+*  This project is based on an example code of UART Full duplex.
 *
-* Related Document: 
+* Related Document:
 * CE210741_UART_Full_Duplex_and_printf_Support_with_PSoC_3_4_5LP.pdf
 *
-* Hardware Dependency: See 
-* CE210741_UART_Full_Duplex_and_printf_Support_with_PSoC_3_4_5LP.pdf
-*
-*******************************************************************************
-* Copyright (2017), Cypress Semiconductor Corporation. All rights reserved.
-*******************************************************************************
-* This software, including source code, documentation and related materials
-* (“Software”), is owned by Cypress Semiconductor Corporation or one of its
-* subsidiaries (“Cypress”) and is protected by and subject to worldwide patent
-* protection (United States and foreign), United States copyright laws and
-* international treaty provisions. Therefore, you may use this Software only
-* as provided in the license agreement accompanying the software package from
-* which you obtained this Software (“EULA”).
-*
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software source
-* code solely for use in connection with Cypress’s integrated circuit products.
-* Any reproduction, modification, translation, compilation, or representation
-* of this Software except as specified above is prohibited without the express
-* written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, 
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED 
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress 
-* reserves the right to make changes to the Software without notice. Cypress 
-* does not assume any liability arising out of the application or use of the 
-* Software or any product or circuit described in the Software. Cypress does 
-* not authorize its products for use in any products where a malfunction or 
-* failure of the Cypress product may reasonably be expected to result in 
-* significant property damage, injury or death (“High Risk Product”). By 
-* including Cypress’s product in a High Risk Product, the manufacturer of such 
-* system or application assumes all risk of such use and in doing so agrees to 
-* indemnify Cypress against all liability. 
 *******************************************************************************/
 
 #include <project.h>
 #include <stdio.h>
 #include "project_common.h"
+
+#include "shell.h"
 
 #include "mrubyc/src/mrubyc.h"
 #include "mmrbc.h"
@@ -66,6 +36,10 @@ the usage of floating point conversion specifier */
 
 uint8 errorStatus = 0u;
 
+static mrbc_tcb *tcb_shell;
+
+#define HEAP_SIZE (1024 * 40 - 1)
+static uint8_t heap[HEAP_SIZE];
 
 /*******************************************************************************
 * Function Name: RxIsr
@@ -83,40 +57,8 @@ uint8 errorStatus = 0u;
 *******************************************************************************/
 CY_ISR(RxIsr)
 {
-  uint8 rxStatus;
-  uint8 rxData;
-  do
-  {
-    /* Read receiver status register */
-    rxStatus = UART_RXSTATUS_REG;
-    if((rxStatus & (UART_RX_STS_BREAK      | UART_RX_STS_PAR_ERROR |
-                    UART_RX_STS_STOP_ERROR | UART_RX_STS_OVERRUN)) != 0u)
-    {
-      /* ERROR handling. */
-      errorStatus |= rxStatus & ( UART_RX_STS_BREAK      | UART_RX_STS_PAR_ERROR |
-                                  UART_RX_STS_STOP_ERROR | UART_RX_STS_OVERRUN);
-    }
-    if((rxStatus & UART_RX_STS_FIFO_NOTEMPTY) != 0u)
-    {
-      /* Read data from the RX data register */
-      rxData = UART_RXDATA_REG;
-      if(errorStatus == 0u)
-      {
-        switch (rxData) {
-          case (0x08): /* '\b' backspace */
-            UART_TXDATA_REG = rxData;
-            UART_TXDATA_REG = ' ';
-            UART_TXDATA_REG = rxData;
-            break;
-          case (0x09): /* '\t' horizontal tab */
-            break;     /* ignore */
-          default:
-            /* Send data backward */
-            UART_TXDATA_REG = rxData;
-        }
-      }
-    }
-  }while((rxStatus & UART_RX_STS_FIFO_NOTEMPTY) != 0u);
+  if (tcb_shell == NULL) return;
+  mrbc_resume_task(tcb_shell);
 }
 
 /*
@@ -126,6 +68,53 @@ int hal_write(int _fd, const void *buf, int nbytes)
 {
   UART_PutArray( buf, nbytes );
   return nbytes;
+}
+
+static void
+c_print(mrbc_vm *vm, mrbc_value *v, int argc)
+{
+  hal_write(1, GET_STRING_ARG(1), strlen((char *)GET_STRING_ARG(1)));
+}
+
+static void
+c_is_fd_empty(mrbc_vm *vm, mrbc_value *v, int argc)
+{
+  uint8 rxStatus;
+  /* Read receiver status register */
+  rxStatus = UART_RXSTATUS_REG;
+  if ((rxStatus & UART_RX_STS_FIFO_NOTEMPTY) != 0u) {
+    SET_FALSE_RETURN();
+  } else {
+    SET_TRUE_RETURN();
+  }
+}
+
+static void
+c_getc(mrbc_vm *vm, mrbc_value *v, int argc)
+{
+  uint8 rxStatus;
+  /* Read receiver status register */
+  rxStatus = UART_RXSTATUS_REG;
+  if((rxStatus & UART_RX_STS_FIFO_NOTEMPTY) != 0u) {
+    /* Read data from the RX data register */
+    char str[] = { UART_RXDATA_REG, '\0' };
+    mrbc_value val = mrbc_string_new(vm, str, 1);
+    SET_RETURN(val);
+  } else {
+    SET_NIL_RETURN();
+  }
+}
+
+static void
+c_exit_shell(mrbc_vm *vm, mrbc_value *v, int argc)
+{
+  /*
+   * PENDING
+   *
+   * you can not call q_delete_task() as it is static function in rrt0.c
+   *
+  q_delete_task(tcb_shell);
+  */
 }
 
 void run(uint8_t *mrb)
@@ -146,13 +135,31 @@ void run(uint8_t *mrb)
   mrbc_value ret = mrbc_send(vm, vm->current_regs, 0, vm->current_regs, "inspect", 0);
   hal_write(1, "=> ", 3);
   hal_write(1, ret.string->data, ret.string->size);
-  hal_write(1, "\n", 1);
+  hal_write(1, "\r\n", 2);
   mrbc_vm_end(vm);
   mrbc_vm_close(vm);
 }
 
-#define HEAP_SIZE (1024 * 40 - 1)
-static uint8_t heap[HEAP_SIZE];
+static void
+c_compile_and_run(mrbc_vm *vm, mrbc_value *v, int argc)
+{
+  static Scope *scope;
+  scope = Scope_new(NULL);
+  StreamInterface *si = StreamInterface_new((char *)GET_STRING_ARG(1), STREAM_TYPE_MEMORY);
+  if (Compile(scope, si)) {
+    run(scope->vm_code);
+    SET_TRUE_RETURN();
+  } else {
+    SET_FALSE_RETURN();
+  }
+  StreamInterface_free(si);
+}
+
+static void
+c_pid(mrbc_vm *vm, mrbc_value *v, int argc)
+{
+  SET_INT_RETURN(0);
+}
 
 /*******************************************************************************
 * Function Name: main()
@@ -176,27 +183,22 @@ int main()
 #endif /* INTERRUPT_CODE_ENABLED == ENABLED */
 
 #if(UART_PRINTF_ENABLED == ENABLED)
-  printf("\r\nprintf() is enabled\r\n\r\n");
+  printf("\r\n\r\nStarting...\r\n\r\n");
 #endif
 
   CyGlobalIntEnable;      /* Enable global interrupts. */
 
-  UART_PutString("Welcome to mmrubyOS!\r\n\r\n");
-  UART_PutString("% puts 'hello world!'\r\n");
-
   mrbc_init(heap, HEAP_SIZE);
 
-  static Scope *scope;
-  scope = Scope_new(NULL);
-  StreamInterface *si = StreamInterface_new("puts 'hello world!'", STREAM_TYPE_MEMORY);
-  if (Compile(scope, si)) {
-    run(scope->vm_code);
-    UART_PutString("% ");
-//    SET_TRUE_RETURN();
-  } else {
-//    SET_FALSE_RETURN();
-  }
-  StreamInterface_free(si);
+  mrbc_define_method(0, mrbc_class_object, "compile_and_run", c_compile_and_run);
+  mrbc_define_method(0, mrbc_class_object, "fd_empty?", c_is_fd_empty);
+  mrbc_define_method(0, mrbc_class_object, "print", c_print);
+  mrbc_define_method(0, mrbc_class_object, "getc", c_getc);
+  mrbc_define_method(0, mrbc_class_object, "pid", c_pid);
+  mrbc_define_method(0, mrbc_class_object, "exit_shell", c_exit_shell);
+
+  tcb_shell = mrbc_create_task(shell, 0);
+  mrbc_run();
 
   for(;;)
   {
